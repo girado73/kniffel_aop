acd2846b254a7199bc1f983e46d3912d91dc57e1
girado73 <ricardoguettner@gmail.com>
2024-07-31 13:10:22 +0200

    Part1 Feldermethoden

--- src/Sheet.java
+++ src/Sheet.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Arrays;
+
 /**
  * Sheet
  * This should represent the Sheet that you have
@@ -46,5 +48,80 @@ public class Sheet {
         "chance: " + String.valueOf(chance) + "\n";
 
     return returnstring;
+  }
+
+  public static int nummercounter(int[] würfel, int nummer) {
+    int resultnumber = 0;
+    for (int number : würfel) {
+      if (number == nummer) {
+        resultnumber += number;
+      }
+    }
+    return resultnumber;
+  }
+
+  /**
+   * Methode welche guckt ob im Array ein Pasch mit vorkommen = anzahl ist
+   * 
+   * @param würfel ist der Array von integern welches die würfel representiert
+   * @param anzahl ist die Anzahl der vorkommnisse der Paschzahl
+   * @param target ist die Zahl nach welcher geguckt wird
+   * @return gibt entweder true zurück wenn der Pasch da ist oder false wenn nicht
+   * @author Ricardo Güttner
+   */
+  private static boolean paschchecker(int[] würfel, int anzahl, int target) {
+    int counter = 0;
+    for (int number : würfel) {
+      if (number == target) {
+        counter++;
+      }
+    }
+    if (counter >= anzahl) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Zähle alle Würfel zusammen falls ein Pasch innerhalb der Würfel ist
+   *
+   * @param würfel  ist der Array von integern welches die würfel representiert
+   * @param paschnr ist die angestrebte Paschzahl
+   * @param target  ist die Zahl nach welcher geguckt wird
+   * @return gibt entweder 0 zurück wenn kein Pasch enthalten ist oder die Summe
+   *         aller würfel
+   * @author Ricardo Güttner
+   */
+  public static int paschcounter(int[] würfel, int paschnr, int target) {
+    int resultnumber = 0;
+    if (paschchecker(würfel, paschnr, target)) {
+      for (int number : würfel) {
+        resultnumber += number;
+      }
+      return resultnumber; // returnt mit summe aller würfel
+    } else {
+      return resultnumber; // returnt mit 0
+    }
+  }
+
+  public static boolean grstrcheck(int[] würfel) {
+    Arrays.sort(würfel);
+    int tmpnumber = würfel[0] - 1;
+    for (int number : würfel) {
+      if (number != tmpnumber + 1) {
+        return false;
+      } else {
+        tmpnumber = number;
+      }
+    }
+    return true;
+  }
+
+  public static boolean klstrcheck(int[] würfel) {
+    Arrays.sort(würfel);
+    int[] vers1 = würfel.; // TODO hier 2 versionen von würfel, einmal ohne den ersten wert
+                          // und einmal ohne den letzten wert
+    int[] vers2 = würfel;
   }
 }
