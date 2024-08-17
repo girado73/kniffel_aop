f0d44674585244804e71a99ef9a21460d228262e
girado73 <ricardoguettner@gmail.com>
2024-08-17 16:33:16 +0200

    Tested Sheed.java

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,99 @@ public class Test {
   public static final String ANSI_RED = "\u001B[31m";
   public static final String ANSI_GREEN = "\u001B[32m";
 
+  private static void errorPrint(String msg) {
+    System.out.println(ANSI_RED + msg + ANSI_RESET);
+  }
+
+  private static void successPrint(String msg) {
+    System.out.println(ANSI_GREEN + msg + ANSI_RESET);
+  }
+
+  private static boolean nummercounter_test() {
+    System.out.println("Nummercounter Test:");
+    int[] würfel = { 1, 2, 4, 4, 4 };
+    int[] würfel2 = { 1, 3, 3, 3, 3 };
+
+    try {
+      assert 3 == src.Sheet.nummercounter(würfel, 4) : "True Assertation Failed";
+      assert 4 == src.Sheet.nummercounter(würfel2, 3) : "True Assertation2 Failed";
+      successPrint("Nummercounter Test Succeeded");
+      return true;
+    } catch (AssertionError e) {
+      errorPrint("Nummercounter Test failed");
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  private static boolean pasch4checker_test() {
+    System.out.println("Pasch4checker Test:");
+    int[] würfelwrong = { 1, 2, 4, 4, 4 };
+    int[] würfeltrue = { 1, 3, 3, 3, 3 };
+
+    try {
+      assert true == src.Sheet.pasch4checker(würfeltrue) : "True Assertation Failed";
+      assert false == src.Sheet.pasch4checker(würfelwrong) : "False Assertation Failed";
+      successPrint("Pasch4checker Test Succeeded");
+      return true;
+    } catch (AssertionError e) {
+      errorPrint("pasch4checker Test failed");
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  private static boolean full_house_check_test() {
+    System.out.println("full_house_check Test:");
+    int[] würfelwrong = { 1, 2, 4, 4, 4 };
+    int[] würfeltrue = { 2, 2, 3, 3, 3 };
+
+    try {
+      assert true == src.Sheet.full_house_check(würfeltrue) : "True Assertation Failed";
+      assert false == src.Sheet.full_house_check(würfelwrong) : "False Assertation Failed";
+      successPrint("full_house_check Test Succeeded");
+      return true;
+    } catch (AssertionError e) {
+      errorPrint("full_house_check Test failed");
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  private static boolean klstrcheck_test() {
+    System.out.println("klstrcheck Test:");
+    int[] würfelwrong = { 1, 2, 4, 4, 4 };
+    int[] würfeltrue = { 2, 1, 2, 3, 4 };
+
+    try {
+      assert true == src.Sheet.klstrcheck(würfeltrue) : "True Assertation Failed";
+      assert false == src.Sheet.klstrcheck(würfelwrong) : "False Assertation Failed";
+      successPrint("klstrcheck Test Succeeded");
+      return true;
+    } catch (AssertionError e) {
+      errorPrint("klstrcheck Test failed");
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
+  private static boolean grstrcheck_test() {
+    System.out.println("grstrcheck Test:");
+    int[] würfelwrong = { 1, 2, 4, 4, 4 };
+    int[] würfeltrue = { 2, 3, 4, 5, 6 };
+
+    try {
+      assert true == src.Sheet.grstrcheck(würfeltrue) : "True Assertation Failed";
+      assert false == src.Sheet.grstrcheck(würfelwrong) : "False Assertation Failed";
+      successPrint("grstrcheck Test Succeeded");
+      return true;
+    } catch (AssertionError e) {
+      errorPrint("grstrcheck Test failed");
+      System.out.println(e.getMessage());
+      return false;
+    }
+  }
+
   private static boolean kniffel_test() {
     System.out.println("Sheet.kniffelcheck Test:");
     int[] kniffel = { 6, 6, 6, 6, 6 };
@@ -55,5 +148,15 @@ public class Test {
     kniffel_test();
     System.out.println("- - - - - - - - - ");
     pasch3_test();
+    System.out.println("- - - - - - - - - ");
+    pasch4checker_test();
+    System.out.println("- - - - - - - - - ");
+    full_house_check_test();
+    System.out.println("- - - - - - - - - ");
+    klstrcheck_test();
+    System.out.println("- - - - - - - - - ");
+    grstrcheck_test();
+    System.out.println("- - - - - - - - - ");
+    nummercounter_test();
   }
 }
