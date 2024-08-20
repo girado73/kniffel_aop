f203187823b88489713b6d3c6a73216e05e56dd4
girado73 <ricardoguettner@gmail.com>
2024-08-20 16:18:51 +0200

    Updated Dice.hs

--- src/Dice.hs
+++ src/Dice.hs
@@ -3,8 +3,9 @@ module Dice where
 import System.Random
 
 digs :: (Integral x) => x -> [x]
-digs 0 = []
-digs x = digs (x `div` 10) ++ [x `mod` 10]
+digs x
+  | x <= 0 = []
+  | otherwise = digs (x `div` 10) ++ [x `mod` 10]
 
 roll :: IO Int
 roll = do
@@ -24,7 +25,10 @@ rollMultiple x = do
 -- bind Notation
 rollMultiple :: Int -> IO [Int]
 rollMultiple 0 = return []
-rollMultiple x =
-  roll >>= \r ->
-    rollMultiple (x - 1) >>= \rs ->
-      return (r : rs)
+rollMultiple x
+  | x > 100 = return [] -- bei mehr lädt es ewig da wir rekursion nehmen
+  | x < 0 = return []
+  | otherwise =
+      roll >>= \r ->
+        rollMultiple (x - 1) >>= \rs ->
+          return (r : rs)
